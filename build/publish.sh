9f31a5a5fb50cd2f0f91f0b97b0e18d7d309819b
3TUSK <urey.s.knowledge@gmail.com>
2021-08-04 02:22:49 -0700

    URLEncode the path because there has been someone using a + which breaks downloading

--- build/publish.sh
+++ build/publish.sh
@@ -7,4 +7,6 @@ ARTIFACT_PATH="2021/ci/build/team-$TEAM_ID/build-$GITHUB_RUN_NUMBER/$ARTIFACT_NA
 aws s3 cp $COMMON_OPTS $ARTIFACT_NAME s3://$ARTIFACT_PATH
 aws s3 cp $COMMON_OPTS $ARTIFACT_NAME.asc s3://$ARTIFACT_PATH.asc
 
-echo "::set-output name=download::https://archive.teacon.cn/$ARTIFACT_PATH"
+ARTIFACT_PATH_ESCAPED=`python3 -c "import urllib.parse; print(urllib.parse.quote('''$ARTIFACT_PATH'''))"`
+
+echo "::set-output name=download::https://archive.teacon.cn/$ARTIFACT_PATH_ESCAPED"
