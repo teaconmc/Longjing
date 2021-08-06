40b216079fb7bad429aac10098b7d3b0d76706eb
3TUSK <urey.s.knowledge@gmail.com>
2021-08-05 17:53:48 -0700

    Experimental: auto-sign mods from extra mod list

---
+++ assemble/update.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+GPG_OPTS='--homedir ./.gnupg --batch --pinentry-mode loopback --passphrase-fd 0 --quiet --armor'
+S3_OPTS="--endpoint-url $S3_ENDPOINT"
+
+# Setup GnuPG
+mkdir -pm 700 ./.gnupg
+cat > teacon.asc <<EOM
+$PGP_KEY
+EOM
+gpg $GPG_OPTS --import teacon.asc <<EOM
+$PGP_PWD
+EOM
+
+for e in `jq -c '.[]' extra.json`; do
+  cat > tmp.json <<EOM
+$e
+EOM
+
+  NAME=`jq -r .name tmp.json`
+  echo Considering $NAME
+  EXISTED=''
+  SIGNED=''
+
+  # Check status of target files.
+  # If mirror=false, then we assume that the file is available.
+  if [ `jq -r .mirror tmp.json` == 'false' ]; then
+    EXISTED=true
+  else
+    aws s3api head-object $S3_OPTS --bucket 2021 --key "ci/extra/$NAME" >/dev/null && EXISTED=true
+  fi
+  aws s3api head-object $S3_OPTS --bucket 2021 --key "ci/extra/$NAME.asc" >/dev/null && SIGNED=true
+
+  # If the file in question exists on remote, skip the uploading part.
+  if [ $EXISTED ]; then
+    echo $NAME already existed on remote, skipping
+    if [ $SIGNED ]; then
+      echo $NAME already signed, skipping
+      continue
+    fi
+  fi
+
+  # Download the missing mod for later use.
+  URI=`jq -r .file tmp.json`
+  curl -s $URI -o $NAME
+
+  # If the file itself doesn't exist, try verify the checksum and upload it.
+  if [ ! $EXISTED ]; then
+    echo `jq -r .sha256 tmp.json` " $NAME" > checksum
+    shasum -a 256 -s -c checksum
+    # If checksum mismatch, then we stop; otherwise upload it to remote.
+    if [ ! $? ]; then
+      echo Check sum failed for $NAME
+      continue
+    else
+      aws s3 cp $S3_OPTS $NAME "s3://2021/ci/extra/$NAME"
+    fi
+  fi
+
+  # If the signature doesn't exist, upload the signature.
+  if [ ! $SIGNED ]; then
+    gpg $GPG_OPTS --local-user TeaCon --detach-sign $NAME <<EOM
+$PGP_PWD
+EOM
+    aws s3 cp $S3_OPTS "$NAME.asc" "s3://2021/ci/extra/$NAME.asc"
+  fi
+
+  rm -f tmp.json checksum
+done
+
+rm -rf teacon.asc .gnupg
