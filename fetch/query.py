c04a6165aa97a6ace98afd23a4e07677d06ee24f
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2022-05-31 18:09:09 +0800

    Update readme file for mods

--- fetch/query.py
+++ fetch/query.py
@@ -141,13 +141,9 @@ def write_team_info(team: Team, contest_name: str, workflow_template: str) -> No
                 f.write(current_head)
 
 def write_readme(team_list: List[Team]):
-    readme='''# TeaCon 2022 参赛团队列表
+    readme = '# TeaCon 2022 参赛团队列表\n\n|团队 ID|团队名|作品名|模组 ID|简介|仓库地址|\n|:------|------:|:------|------|:------|------|'
 
-    |团队 ID|团队名 |作品名 |Mod ID|简介   |仓库地址|
-    |:------|------:|:------|------|:------|------|
-    '''
-
-    readme+='\n'.join([ f"|{t['id']}|{t['name']}|{t['work_name']}|`{t['work_id']}`|" + t['work_description'].replace('\n', '<br />') + f"|{t['repo']}|" for t in team_list ])
+    readme += '\n'.join([ f"|{t['id']}|{t['name']}|{t['work_name']}|`{t['work_id']}`|" + t['work_description'].replace('\n', '<br />') + f"|{t['repo']}|" for t in team_list ])
 
     with open('mods/README.md', 'w+') as f:
         f.write(readme)
