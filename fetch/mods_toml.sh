8030a95ee16dd92fd788bc7aebfc34fe2fd5d6a4
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2022-05-31 23:43:53 +0800

    Add a new line break for printing encoded mods.toml

--- fetch/mods_toml.sh
+++ fetch/mods_toml.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 unzip -p $ARTIFACT_PATH META-INF/mods.toml | tee mods.toml
+
+echo # some mods.toml file does not have a line break at the end
 echo ::set-output name=base64::`base64 -w 0 mods.toml`
