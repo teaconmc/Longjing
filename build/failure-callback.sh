56d47de72a7c8d3e52d9192b88233674e056e0d9
3TUSK <git@tritusk.info>
2023-06-30 21:22:20 -0700

    Send notification to team members on build failure

---
+++ build/failure-callback.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat > payload.json <<MSG
+{
+    "subject": "[TeaCon 2023] $TEAM_ID 构建失败通知",
+    "content": "TeaCon 2023 参与者您好：\n\n您的作品（模组 ID $TEAM_ID）在最新一次构建（第 $GITHUB_RUN_NUMBER 次构建）中出错，未能产出 jar 文件。\n您可以通过该 URL 获得构建详情：$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID \n如有疑问，请联系 TeaCon 执行委员会成员。\n\n\n顺颂时祺\nTeaCon 执行委员会",
+    "teams": [ $TEAM_SEQ ]
+}
+MSG
+
+curl -s -X POST -d @payload.json -H "Authorization: Bearer $BILUOCHUN_TOKEN" $BILUOCHUN_URL/api/v2/notify
