b9855573d63f42c8fc8ac81ce12dc340aa0deba9
3TUSK <git@tritusk.info>
2023-07-25 13:15:34 -0700

    Suppress which(1) output; add more comments

--- build/build.sh
+++ build/build.sh
@@ -15,7 +15,7 @@ CURL_OPTIONS=( --no-progress-meter -S -f -o ./longjing-config.json )
 
 curl ${CURL_OPTIONS[@]} $LONGJING_CONFIG_URL || die Fail to fetch Longjing cnofig
 
-which jq || die Cannot find program jq, build process cannot continue
+which -s jq || die Cannot find program jq, build process cannot continue
 
 # Retrieve special build command, fallback to 'build' if not specified
 BUILD_COMMAND=$(jq -rM .build_command ./longjing-config.json)
@@ -50,4 +50,6 @@ fi
 # Here used to be --max-workers=1 to workaround an recurring issue regarding reobf failure.
 # However, according to https://github.com/MinecraftForge/ForgeGradle/pull/755, the issue should have been fixed. 
 # If we observe anything similar to theMinecraftForge/ForgeGradle#697 again, add it back.
+# We add empty socks.proxyHost, http.proxyHost and https.proxyHost system properties, so that any pre-existing 
+# proxy configurations are void in CI environment. We do not need any proxy on GitHub Action.
 TEACON_ARTIFACT_TASK=$OUTPUT_JAR_TASK $GRADLE_EXEC -Dsocks.proxyHost= -Dhttp.proxyHost= -Dhttps.proxyHost= --stacktrace -I ../setup.gradle "${BUILD_COMMAND[@]}" teaconLongjingProcessing
