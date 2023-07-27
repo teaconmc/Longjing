8cc618cdaccbf0f407aa12b06c9e0b2d02217679
3TUSK <git@tritusk.info>
2023-07-26 19:13:22 -0700

    Move size check to mods_toml.py

--- build/mods_toml.py
+++ build/mods_toml.py
@@ -25,3 +25,13 @@ with zipfile.ZipFile(os.environ['ARTIFACT_LOCAL_PATH']) as z:
                 "defined in mods.toml does not contains any mod id matching the team id",
                 f"({team_id}) so this build will fail", flush=True)
             exit(-1)
+
+file_size=os.path.getsize(os.environ['ARTIFACT_LOCAL_PATH'])
+if file_size > 10 * 1024 * 1024: # 10485760 bytes, aka 10MiB
+    if os.path.exists('./mods/team-' + team_id.replace('_', '-') + '/oversize-permit'):
+        print(f"::notice::Build output exceeds the 10MiB limit (actual size {file_size} bytes); permission has acquired to exceed this limit")
+    else:
+        print(f"::error::Build output exceeds the 10MiB limit (actual size {file_size} bytes); however, the mod has not acquired explicit permission for exceeded size")
+        exit(-1)
+
+print("Preliminary check has all passed.")
