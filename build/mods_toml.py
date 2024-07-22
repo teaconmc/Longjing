d379a1822eb73f29edc067fff78e329d9f3032b1
3TUSK <git@tritusk.info>
2024-07-21 19:39:09 -0700

    oh sh*t we are using neoforge.mods.toml now

--- build/mods_toml.py
+++ build/mods_toml.py
@@ -14,15 +14,15 @@ if not team_id:
     print('Team id is not found or is empty, which should not happen!')
     exit(-1)
 
-# Check if the mod id declared in Biluochun exists in mods.toml
+# Check if the mod id declared in Biluochun exists in neoforge.mods.toml
 # Fails if not found.
 with zipfile.ZipFile(os.environ['ARTIFACT_LOCAL_PATH']) as z:
-    with z.open('META-INF/mods.toml') as f:
+    with z.open('META-INF/neoforge.mods.toml') as f:
         metadata=tomllib.load(f)
         mods=metadata.get('mods', [])
         if not any(mod.get('modId', '').replace('_', '-') == team_id for mod in mods):
             print(f"::warning::The mod id list {[mod.get('modId', '') for mod in mods]}",
-                "defined in mods.toml does not contains any mod id matching the team id",
+                "defined in neoforge.mods.toml does not contains any mod id matching the team id",
                 f"({team_id}) so this build will fail", flush=True)
             exit(-1)
         license=metadata.get('license', None)
