696cb0e39f5e540267dd34c4a64307177ddab6a0
龙井 [TeaCon CI Service] <75922763+teacon-bot[bot]@users.noreply.github.com>
2024-08-31 07:57:13 +0000

    Sat Aug 31 07:57:13 UTC 2024: trigger new build(s)

--- mods/batch-clone.sh
+++ mods/batch-clone.sh
@@ -2,228 +2,228 @@
 
 # 「TeaCon 甲辰」 submission repositories batch clone script
 
-# Clone 友谊徽章 (modid = team['work_id']) 
+# Clone 友谊徽章 (modid = friendship_bracelet) 
 git clone https://github.com/teaconmc/FriendshipBracelet.git friendship_bracelet
 
-# Clone 苇原 (modid = team['work_id']) 
+# Clone 苇原 (modid = ashihara) 
 git clone https://github.com/Kogasa-Studio/Ashihara ashihara
 
-# Clone 马儿蹦跳 (modid = team['work_id']) 
+# Clone 马儿蹦跳 (modid = umapyoi) 
 git clone https://github.com/0999312/umapyoi umapyoi
 
-# Clone 东方夜雀食堂 (modid = team['work_id']) 
+# Clone 东方夜雀食堂 (modid = mystia_izakaya) 
 git clone https://github.com/Crystal1921/mystias_izakaya mystia_izakaya
 
-# Clone 随处随笔 WorldComment (modid = team['work_id']) 
+# Clone 随处随笔 WorldComment (modid = worldcomment) 
 git clone https://github.com/zbx1425/world-comment worldcomment
 
-# Clone CrystalDream (modid = team['work_id']) 
+# Clone CrystalDream (modid = crystaldream) 
 git clone https://github.com/AmarokIce/CrystalDream-NeoForge crystaldream
 
-# Clone 贴贴 (modid = team['work_id']) 
+# Clone 贴贴 (modid = nestle) 
 git clone https://github.com/KunoSayo/Nestle nestle
 
-# Clone Crock Pot (modid = team['work_id']) 
+# Clone Crock Pot (modid = crockpot) 
 git clone https://github.com/SihenZhang/CrockPot crockpot
 
-# Clone 真正的和平模式 (modid = team['work_id']) 
+# Clone 真正的和平模式 (modid = real_peaceful_mode) 
 git clone https://github.com/Viola-Siemens/Real-Peaceful-Mode real_peaceful_mode
 
-# Clone 地火 (modid = team['work_id']) 
+# Clone 地火 (modid = undergroundfire) 
 git clone https://github.com/PositiveEStudio/undergroundfire undergroundfire
 
-# Clone 打铁匠人(Ding Mod) (modid = team['work_id']) 
+# Clone 打铁匠人(Ding Mod) (modid = ding) 
 git clone https://github.com/NitianStudio/DingMod ding
 
-# Clone 分茶 (modid = team['work_id']) 
+# Clone 分茶 (modid = caupona) 
 git clone https://github.com/TeamMoegMC/Caupona caupona
 
-# Clone 会饮 (modid = team['work_id']) 
+# Clone 会饮 (modid = convivium) 
 git clone https://github.com/khjxiaogu/Convivium convivium
 
-# Clone MadParticle (modid = team['work_id']) 
+# Clone MadParticle (modid = madparticle) 
 git clone https://github.com/USS-Shenzhou/MadParticle madparticle
 
-# Clone Guide Me To (modid = team['work_id']) 
+# Clone Guide Me To (modid = guide_me_to) 
 git clone https://github.com/USS-Shenzhou/GuideMeTo guide_me_to
 
-# Clone  EveryXHotpot一起火锅 (modid = team['work_id']) 
+# Clone  EveryXHotpot一起火锅 (modid = everyxhotpot) 
 git clone  https://github.com/Argon4W/EveryXHotpot/ everyxhotpot
 
-# Clone 微光纪事(GlimmeringTales) (modid = team['work_id']) 
+# Clone 微光纪事(GlimmeringTales) (modid = glimmeringtales) 
 git clone https://github.com/Minecraft-LightLand/GlimmeringTales glimmeringtales
 
-# Clone 料理乐事(Cuisine-Delight) (modid = team['work_id']) 
+# Clone 料理乐事(Cuisine-Delight) (modid = cuisinedelight) 
 git clone https://github.com/Minecraft-LightLand/Cuisine-Delight cuisinedelight
 
-# Clone 爽滑满天：甲辰版 PipeSlide - the First Heavenly Stem and the Fifth Earthly Branch in the Chinese Sexagenary Cycle Edition (modid = team['work_id']) 
+# Clone 爽滑满天：甲辰版 PipeSlide - the First Heavenly Stem and the Fifth Earthly Branch in the Chinese Sexagenary Cycle Edition (modid = pipeslide) 
 git clone https://github.com/MarbleGateKeeper/PipeSlide-the-First-Heavenly-Stem-and-the-Fifth-Earthly-Branch-in-the-Chinese-Sexagenary-Cycle-ed. pipeslide
 
-# Clone 随想自然 (modid = team['work_id']) 
+# Clone 随想自然 (modid = randomsonnature) 
 git clone https://github.com/XiaoPangxie732/RandomsOnNature randomsonnature
 
-# Clone 明日之花 (modid = team['work_id']) 
+# Clone 明日之花 (modid = higanbana) 
 git clone https://github.com/qyl27/Higanbana.git higanbana
 
-# Clone 工业基石 (modid = team['work_id']) 
+# Clone 工业基石 (modid = industrybase) 
 git clone https://github.com/BinZhengStudio/IndustryBase industrybase
 
-# Clone 流年年的无想 (modid = team['work_id']) 
+# Clone 流年年的无想 (modid = meteornoidea) 
 git clone gitee.com/openharmony meteornoidea
 
-# Clone 享尝 (modid = team['work_id']) 
+# Clone 享尝 (modid = doutdes) 
 git clone https://github.com/khjxiaogu/Doutdes doutdes
 
-# Clone 旋律魔法 (modid = team['work_id']) 
+# Clone 旋律魔法 (modid = melodymagic) 
 git clone https://github.com/StrangeSmell/MelodyMagic melodymagic
 
-# Clone 菌丝工艺(HyphaCraft) (modid = team['work_id']) 
+# Clone 菌丝工艺(HyphaCraft) (modid = hyphacraft) 
 git clone https://github.com/Chlamydomonos/HyphaCraft hyphacraft
 
-# Clone 永远的朋友 / Friendship Forever (modid = team['work_id']) 
+# Clone 永远的朋友 / Friendship Forever (modid = friendship_forever) 
 git clone https://github.com/LucunJi/friendship-forever friendship_forever
 
-# Clone NeoMTR 纸板箱特色我的世界铁路 (modid = team['work_id']) 
+# Clone NeoMTR 纸板箱特色我的世界铁路 (modid = mtr) 
 git clone https://github.com/zbx1425/Minecraft-Transit-Railway mtr
 
-# Clone 欢庆 (modid = team['work_id']) 
+# Clone 欢庆 (modid = celebrations) 
 git clone https://github.com/LeoMinecraftModding/Celebrations-Mod celebrations
 
-# Clone 更好的打雪仗3 (modid = team['work_id']) 
+# Clone 更好的打雪仗3 (modid = bsf) 
 git clone https://github.com/linngdu664/Better-Snowball-Fight3-NeoForge bsf
 
-# Clone 头颅工艺 (modid = team['work_id']) 
+# Clone 头颅工艺 (modid = skullcraft) 
 git clone https://github.com/Viola-Siemens/SkullCraft skullcraft
 
-# Clone 深岩银河激光指示器 (modid = team['work_id']) 
+# Clone 深岩银河激光指示器 (modid = drglaserpointer) 
 git clone https://github.com/linngdu664/DRG-Laser-Pointer-Mod drglaserpointer
 
-# Clone 弹幕聊天 (modid = team['work_id']) 
+# Clone 弹幕聊天 (modid = bchat) 
 git clone https://github.com/LNatit/BulletChat bchat
 
-# Clone 不死灵药 (modid = team['work_id']) 
+# Clone 不死灵药 (modid = the_elixir) 
 git clone https://github.com/TT432/TheElixir the_elixir
 
-# Clone Mine Team (modid = team['work_id']) 
+# Clone Mine Team (modid = mine_team) 
 git clone https://github.com/XiaoHuNao/MineTeam mine_team
 
-# Clone 百般武艺 (modid = team['work_id']) 
+# Clone 百般武艺 (modid = assorted_armaments) 
 git clone https://github.com/LeoMinecraftModding/assorted-armaments assorted_armaments
 
-# Clone 元素论 (modid = team['work_id']) 
+# Clone 元素论 (modid = elemental_theory) 
 git clone https://gitee.com/uicdb/ElementalTheory2 elemental_theory
 
-# Clone BC的远征物语 (modid = team['work_id']) 
+# Clone BC的远征物语 (modid = bcs_expedition_world) 
 git clone https://github.com/LeoLezury/bcs-expedition-world bcs_expedition_world
 
-# Clone 一起跳舞！ (modid = team['work_id']) 
+# Clone 一起跳舞！ (modid = everyxdance) 
 git clone https://github.com/Viola-Siemens/EveryXDance everyxdance
 
-# Clone 橙王的权杖 (modid = team['work_id']) 
+# Clone 橙王的权杖 (modid = staff_of_the_king_orange) 
 git clone https://github.com/Glyceryl6/StaffOfTheKingOrange staff_of_the_king_orange
 
-# Clone 浅草轻语 WhisperGrove (modid = team['work_id']) 
+# Clone 浅草轻语 WhisperGrove (modid = whispergrove) 
 git clone https://github.com/HiedaCamellia/WhisperGrove whispergrove
 
-# Clone 自然通路 (modid = team['work_id']) 
+# Clone 自然通路 (modid = natural_transmute) 
 git clone https://github.com/Glyceryl6/NaturalTransmute natural_transmute
 
-# Clone 糖果世界社区版 (modid = team['work_id']) 
+# Clone 糖果世界社区版 (modid = candycraftce) 
 git clone https://github.com/Bread-NiceCat/CandyCraftCE candycraftce
 
-# Clone 节气 (modid = team['work_id']) 
+# Clone 节气 (modid = eclipticseasons) 
 git clone https://github.com/joe-vettek/Ecliptic eclipticseasons
 
-# Clone 墨迹工艺 (modid = team['work_id']) 
+# Clone 墨迹工艺 (modid = inkraft) 
 git clone https://github.com/qyl27/Inkraft.git inkraft
 
-# Clone 草工艺 (modid = team['work_id']) 
+# Clone 草工艺 (modid = grass_craft) 
 git clone https://github.com/ALittleBot/GrassCraft grass_craft
 
-# Clone 土豆力量 (modid = team['work_id']) 
+# Clone 土豆力量 (modid = potatopower) 
 git clone https://github.com/langxsy/potatopower potatopower
 
-# Clone 对决 (modid = team['work_id']) 
+# Clone 对决 (modid = cardduel) 
 git clone https://github.com/mcczai/neoforge-1.21-board_games cardduel
 
-# Clone 傀儡装配(Modular Golems) (modid = team['work_id']) 
+# Clone 傀儡装配(Modular Golems) (modid = modulargolems) 
 git clone https://github.com/Minecraft-LightLand/ModularGolems modulargolems
 
-# Clone 酵化 (modid = team['work_id']) 
+# Clone 酵化 (modid = ferment) 
 git clone https://github.com/TT432/ferment ferment
 
-# Clone 纸带八音盒 (modid = team['work_id']) 
+# Clone 纸带八音盒 (modid = cc_mb) 
 git clone https://github.com/C20C01/MusicBox cc_mb
 
-# Clone 星云纪事 (modid = team['work_id']) 
+# Clone 星云纪事 (modid = nch) 
 git clone https://github.com/Zoshsgahdnkc/NebulaChronicles-NeoForge nch
 
-# Clone 胡萝卜厨房 (modid = team['work_id']) 
+# Clone 胡萝卜厨房 (modid = kitchenkarrot) 
 git clone https://github.com/Zoshsgahdnkc/kitchenkarrot/ kitchenkarrot
 
-# Clone 夜雀乐事Mystias Izakaya (modid = team['work_id']) 
+# Clone 夜雀乐事Mystias Izakaya (modid = mystias_izakaya) 
 git clone https://github.com/HiedaCamellia/MystiasIzakaya mystias_izakaya
 
-# Clone Furniture Plan (modid = team['work_id']) 
+# Clone Furniture Plan (modid = furnitureplan) 
 git clone https://github.com/MakerTechno/Forge-FurniturePlan/ furnitureplan
 
-# Clone 储液抽屉：遗产 (modid = team['work_id']) 
+# Clone 储液抽屉：遗产 (modid = fluiddrawerslegacy) 
 git clone https://github.com/joe-vettek/Fluid-Drawers-Legacy fluiddrawerslegacy
 
-# Clone 更好的单方块 (modid = team['work_id']) 
+# Clone 更好的单方块 (modid = oneblock) 
 git clone https://github.com/joe-vettek/One-Block oneblock
 
-# Clone 清洁能源 (modid = team['work_id']) 
+# Clone 清洁能源 (modid = cleanenergy) 
 git clone https://github.com/FengMingMC/CleanEnergy.git cleanenergy
 
-# Clone 更好的派对 (modid = team['work_id']) 
+# Clone 更好的派对 (modid = betterparty) 
 git clone https://gitee.com/WangLa0Sh1/BetterParty betterparty
 
-# Clone ThatSkyInteractions (modid = team['work_id']) 
+# Clone ThatSkyInteractions (modid = thatskyinteractions) 
 git clone https://github.com/LouisQuepierts/ThatSkyInteractions thatskyinteractions
 
-# Clone 抱我！(HugMe!) (modid = team['work_id']) 
+# Clone 抱我！(HugMe!) (modid = hugme) 
 git clone https://github.com/TunYuntuwuQWQ/HugMe hugme
 
-# Clone 伊卡洛斯之翼 (modid = team['work_id']) 
+# Clone 伊卡洛斯之翼 (modid = locusazzurro_icaruswings) 
 git clone https://github.com/LocusAzzurro/IcarusWings locusazzurro_icaruswings
 
-# Clone 茶风·纪事：追叙 (modid = team['work_id']) 
+# Clone 茶风·纪事：追叙 (modid = teastory) 
 git clone https://github.com/joe-vettek/Tea-the-Story teastory
 
-# Clone 异常乐事 (modid = team['work_id']) 
+# Clone 异常乐事 (modid = anomaly_delight) 
 git clone https://github.com/USS-Shenzhou/AnomalyDelight anomaly_delight
 
-# Clone 自由相机API (modid = team['work_id']) 
+# Clone 自由相机API (modid = freecameraapi) 
 git clone https://gitee.com/anecansaitin/FreeCameraAPI.git freecameraapi
 
-# Clone 车万女仆 (modid = team['work_id']) 
+# Clone 车万女仆 (modid = touhou_little_maid) 
 git clone https://github.com/TartaricAcid/TouhouLittleMaid touhou_little_maid
 
-# Clone 极光幽境2 (modid = team['work_id']) 
+# Clone 极光幽境2 (modid = theaurorian) 
 git clone https://github.com/TeamPancakeMC/TheAurorian theaurorian
 
-# Clone 岩浆钓鱼 (modid = team['work_id']) 
+# Clone 岩浆钓鱼 (modid = lavafishing) 
 git clone https://github.com/TeamSunset/LavaFishing lavafishing
 
-# Clone 网络音乐机 (modid = team['work_id']) 
+# Clone 网络音乐机 (modid = netmusic) 
 git clone https://github.com/TartaricAcid/NetMusic netmusic
 
-# Clone 双倍快乐！ (modid = team['work_id']) 
+# Clone 双倍快乐！ (modid = double_the_happiness) 
 git clone https://github.com/haohao-0407/double_the_happiness.git double_the_happiness
 
-# Clone LoveMod:Furina (modid = team['work_id']) 
+# Clone LoveMod:Furina (modid = lovemod) 
 git clone https://github.com/Bread-NiceCat/LoveModFurina lovemod
 
-# Clone 漆MOD-1.21 (modid = team['work_id']) 
+# Clone 漆MOD-1.21 (modid = urushi) 
 git clone https://github.com/HiedaCamellia/Urushi-MOD-1.21 urushi
 
-# Clone 方块小镇 (modid = team['work_id']) 
+# Clone 方块小镇 (modid = yuushya) 
 git clone https://gitee.com/yuushyatownscape/yuushya-townscape yuushya
 
-# Clone 大宝刀 (modid = team['work_id']) 
+# Clone 大宝刀 (modid = dabaosword) 
 git clone https://github.com/Amotassic/DabaoSword-NeoForge dabaosword
 
-# Clone Mino++ (modid = team['work_id']) 
+# Clone Mino++ (modid = minopp) 
 git clone https://github.com/zbx1425/Minopp minopp
 
