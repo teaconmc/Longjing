ddd477fa3edf99a25aa2f9ed1504f03a27f63df6
龙井 [TeaCon CI Service] <75922763+teacon-bot[bot]@users.noreply.github.com>
2026-05-07 06:34:36 +0000

    Thu May  7 06:34:36 UTC 2026: trigger new build(s)

---
+++ mods/batch-clone.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+# 「TeaCon 2026」 submission repositories batch clone script
+
+# Clone 交换玩偶（Swap A Doll） (modid = swapadoll) 
+git clone https://github.com/teaconmc/SwapADoll.git swapadoll
+
+# Clone 贴贴 (modid = nestle) 
+git clone https://github.com/KunoSayo/nestle nestle
+
+# Clone 工业基石 (modid = industrybase) 
+git clone https://github.com/BinZhengStudio/IndustryBase industrybase
+
+# Clone AAA Particles: World (modid = aaa_particles_world) 
+git clone https://github.com/ChloePrime/AAA-Particles-World aaa_particles_world
+
+# Clone 有借无还 (modid = lendandregret) 
+git clone https://gitea.bot.leisuretimedock.top/R3944Realms/LendAndRegret lendandregret
+
+# Clone 家具计划 (modid = furnitureplan) 
+git clone https://github.com/MakerTechno/Forge-FurniturePlan/ furnitureplan
+
+# Clone ThatSkyInteractions (modid = thatskyinteractions) 
+git clone https://github.com/LouisQuepierts/ThatSkyInteractions thatskyinteractions
+
+# Clone 超级拴绳 (modid = superlead) 
+git clone https://gitea.bot.leisuretimedock.top/R3944Realms/SuperLeadRope superlead
+
+# Clone ConduitStratus (modid = conduit_stratus) 
+git clone https://github.com/vfyjxf/ConduitStratus conduit_stratus
+
+# Clone 铁砧工艺 (modid = anvilcraft) 
+git clone https://github.com/Anvil-Dev/AnvilCraft anvilcraft
+
+# Clone x3d map (modid = x3d_map) 
+git clone https://github.com/xkball/x3dmap x3d_map
+
+# Clone Blueteath (modid = blueteath) 
+git clone https://github.com/Gebardensprache/Blueteath blueteath
+
+# Clone MadParticle (modid = madparticle) 
+git clone https://github.com/USS-Shenzhou/MadParticle madparticle
+
+# Clone Channel (modid = channel) 
+git clone https://github.com/USS-Shenzhou/Channel channel
+
+# Clone 我想加入天际俯冲 Skyward Dive (modid = skywarddive) 
+git clone https://github.com/MarbleGateKeeper/SkywardDive skywarddive
+
+# Clone Roads (modid = roads) 
+git clone https://github.com/xkball/roads roads
+
+# Clone 蜂会 (modid = beecrasy) 
+git clone https://github.com/khjxiaogu/Beecrasy beecrasy
+
+# Clone 苇原 (modid = ashihara) 
+git clone https://github.com/Kogasa-Studio/Ashihara ashihara
+
+# Clone 袖里舆图 (modid = aleeve_atlas) 
+git clone https://github.com/Gu-ZT/aleeve-atlas aleeve_atlas
+
+# Clone 铁砧工艺：猪+ (modid = anvilcraft_pigsplus) 
+git clone https://github.com/TBPig/AnvilCraft-PigsPlus anvilcraft_pigsplus
+
+# Clone 进阶四叶草 Advanced Clover (modid = advanced_clover) 
+git clone https://github.com/luoxueyasha/AdvancedClover advanced_clover
+
+# Clone V我50 (modid = vw50) 
+git clone https://github.com/Glyceryl6/vw50-26.1.2 vw50
+
+# Clone 收获：遗产 (modid = harvestheritage) 
+git clone https://github.com/theabab23333/HarvestHeritage harvestheritage
+
+# Clone NeoMystiasIzakaya (modid = neo_mystias_izakaya) 
+git clone https://github.com/Crystal1921/NeoMystiasIzakaya neo_mystias_izakaya
+
+# Clone Mekanism:More Machine (modid = mekmm) 
+git clone https://github.com/lostmyself8/Mekanism-MoreMachine mekmm
+
+# Clone 八角 (modid = anisum) 
+git clone https://github.com/Anvil-Dev/Anisum anisum
+
+# Clone 傀儡装配（Modular Golems） (modid = modulargolems) 
+git clone https://github.com/Minecraft-LightLand/ModularGolems modulargolems
+
+# Clone 红包🧧 (modid = redenvelope) 
+git clone https://gitea.bot.leisuretimedock.top/Leisuretimedock/RedEnvelope redenvelope
+
+# Clone 急 (modid = hurriedness) 
+git clone https://github.com/XiaoPangxie732/Hurriedness hurriedness
+
+# Clone MMod (modid = mmod) 
+git clone https://github.com/wiyuka0/mmod mmod
+
+# Clone 春日影 (modid = vernalecho) 
+git clone https://github.com/PickAID/VernalEcho vernalecho
+
+# Clone 我的肉鸽 (modid = beyond) 
+git clone https://github.com/Ver-shift/SubModulesGeneHunter beyond
+
+# Clone 他们喊着友情啊羁绊啊未来啊什么的就冲上来了 Power of Friendship (modid = power_of_friendship) 
+git clone https://github.com/MarbleGateKeeper/PowerOfFriendship power_of_friendship
+
+# Clone 圣脾 (modid = shengpi) 
+git clone https://github.com/TT432/shengpi shengpi
+
+# Clone 卡里普索的糖果工坊 (modid = ccw) 
+git clone https://github.com/LNatit/CandyWorkshop ccw
+
+# Clone 中键标点 (modid = middle_key_ping) 
+git clone https://github.com/Anvil-Dev/MiddleKeyPing middle_key_ping
+
+# Clone 山河同契 (modid = bonded_peaks) 
+git clone https://github.com/Gu-ZT/bonded-peaks bonded_peaks
+
+# Clone 最后的上勾拳 One Last Rising Uppercut (modid = olru) 
+git clone https://github.com/MarbleGateKeeper/OneLastRisingUppercut olru
+
+# Clone 铁砧工艺：猫+! (modid = nekoplus) 
+git clone https://github.com/ZhuRuoLing/NekoPlus nekoplus
+
+# Clone NeoMTR 纸板箱特色我的世界铁路 (modid = mtr) 
+git clone https://github.com/zbx1425/NeoMTR mtr
+
+# Clone 随处随笔 WorldComment (modid = worldcomment) 
+git clone https://github.com/zbx1425/world-comment worldcomment
+
+# Clone Mino++ (modid = minopp) 
+git clone https://github.com/zbx1425/minopp minopp
+
+# Clone MinecartRevolution (modid = minecartrevolution) 
+git clone https://github.com/hotpad100c/mincartrevolution-neo minecartrevolution
+
+# Clone 本质主义 (modid = essentialism) 
+git clone https://github.com/Polari-Stars-MC/essentialism essentialism
+
+# Clone 泰拉巨石 (modid = terraria_boulders) 
+git clone https://github.com/Magic-team-jvav/TerrariaBoulders terraria_boulders
+
+# Clone 伊卡洛斯之翼 (modid = locusazzurro_icaruswings) 
+git clone https://github.com/LocusAzzurro/IcarusWings locusazzurro_icaruswings
+
+# Clone 藿香 (modid = ageratum) 
+git clone https://github.com/Anvil-Dev/Ageratum ageratum
+
+# Clone 以太工艺 (modid = ether_craft) 
+git clone https://github.com/FantasyIT-Projects/ether-craft-neo/settings ether_craft
+
+# Clone 来自深渊：弹药包 (modid = cartridge) 
+git clone https://github.com/Chlamydomonos/Cartridge cartridge
+
+# Clone Programmable Magic (modid = programmable_magic) 
+git clone https://github.com/creepebucket/programmable_magic programmable_magic
+
+# Clone 回声透镜 Echo Lens (modid = echolens) 
+git clone https://github.com/MarbleGateKeeper/VibeModding2026May echolens
+
+# Clone 我们意念合一 (modid = ourthoughtsareone) 
+git clone https://github.com/WhiseNT/OurThoughtsAreOne.git ourthoughtsareone
+
+# Clone 和弦 (modid = kord) 
+git clone https://github.com/LNatit/KORD kord
+
+# Clone Anesthesia (modid = anesthesia) 
+git clone https://github.com/CNDRU/Anesthesia.git anesthesia
+
+# Clone 群青 (modid = ultramarine) 
+git clone https://github.com/LocusAzzurro/Ultramarine ultramarine
+
