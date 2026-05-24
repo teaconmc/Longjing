03b375834e40bca0793615adecae8e0e7e6f1f5c
龙井 [TeaCon CI Service] <75922763+teacon-bot[bot]@users.noreply.github.com>
2026-05-24 08:36:02 +0000

    Sun May 24 08:36:02 UTC 2026: trigger new build(s)

--- mods/batch-clone.sh
+++ mods/batch-clone.sh
@@ -161,9 +161,6 @@ git clone https://github.com/Tower-of-Sighs/SpringMahjong springmahjong
 # Clone 群青 (modid = ultramarine) 
 git clone https://github.com/LocusAzzurro/Ultramarine ultramarine
 
-# Clone 吾有一術 (modid = wenyan_programming) 
-git clone https://github.com/gyxx-xc/WenyanNature wenyan_programming
-
 # Clone 方块小镇 (modid = yuushya) 
 git clone https://gitee.com/yuushyatownscape/yuushya-townscape/ yuushya
 
