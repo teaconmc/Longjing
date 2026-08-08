938636c5dc112bccab3820347377e40509e8a48a
龙井 [TeaCon CI Service] <75922763+teacon-bot[bot]@users.noreply.github.com>
2026-08-08 08:16:56 +0000

    Sat Aug  8 08:16:56 UTC 2026: trigger new build(s)

--- mods/batch-clone.sh
+++ mods/batch-clone.sh
@@ -251,6 +251,9 @@ git clone https://github.com/XiaoPangxie732/Grack grack
 # Clone NetherLink (modid = netherlink) 
 git clone https://github.com/MUYUTwilighter/NetherLink netherlink
 
+# Clone 兰法斯：本德森之章 (modid = lanfasie_benderson) 
+git clone https://github.com/cvrunmin/LanfasieBenderson lanfasie_benderson
+
 # Clone 动量 (modid = momentum) 
 git clone https://github.com/AkiraHane/Momentum momentum
 
